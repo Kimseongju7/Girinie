2f2dad5eccfbbb2af9bba2ebf98d9abaac3e1edb
Youngju Lee <oortmealy@gmail.com>
2025-07-01 18:11:31 +0900

    WIP: AppRouter conflict 수정 중

--- Girinie-frontend/src/router/AppRouter.tsx
+++ Girinie-frontend/src/router/AppRouter.tsx
@@ -1,28 +1,32 @@
 // AppRouter.tsx
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "@/components/common/Header";
 import HomePage from "@/pages/home/HomePage.tsx";
 import LoginPage from "@/pages/login/LoginPage";
 import SignupPage from "@/pages/signup/SignupPage";
 import GuardianPage from "@/pages/gardian/GuardianPage";
 import ChildPage from "@/pages/child/ChildPage";
-import FindAccountPage from "@/pages/account/FindAccountPage";
-import ParentCalendar from "@/pages/calendar/ParentCalendar";
-import KidCalendar from "@/pages/calendar/KidCalendar";
+import SettingsPage from "@/pages/settings/SettingPage";
+import ReportPage from "@/pages/report/ReportPage";
+import ChatbotPage from "@/pages/chatbot/ChatBotPage";
+import LearningRagePage from "@/pages/learningrate/LearningRatePage";
+
 // import GuardianLayout from '@/components/layout/GuardianLayout';
 
 export default function AppRouter() {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/signup" element={<SignupPage />} />
-        <Route path="guardian" element={<GuardianPage />} />
-        <Route path="child" element={<ChildPage />} />
-        <Route path="/find-account" element={<FindAccountPage />} />
-        <Route path="/calendar/parent" element={<ParentCalendar />} />
-        <Route path="/calendar/kid" element={<KidCalendar />} />
-        {/* 추가로 여기다가 report, settings도 계속 추가 */}
+        <Route path="/gardian" element={<GuardianPage />} />
+        <Route path="/child" element={<ChildPage />} />
+        <Route path="/report" element={<ReportPage />} />
+        <Route path="/settings" element={<SettingsPage />} />
+        <Route path="/chatbot" element={<ChatbotPage />} />
+        <Route path="/learning-rate" element={<LearningRagePage />} />
       </Routes>
     </BrowserRouter>
   );
