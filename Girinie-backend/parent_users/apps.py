e7b0686468c183377ae962f7056dd7a148f0fb00
Seongju Kim <123197704+Kimseongju7@users.noreply.github.com>
2025-07-04 18:42:41 +0900

    Revert "[chore/#7] auth API 문서 자동화 및 회원탈퇴 기능 추가, View 리네이밍"

--- Girinie-backend/parent_users/apps.py
+++
@@ -1,7 +0,0 @@
-from django.apps import AppConfig
-
-
-class ParentUserConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'parent_users'
-    verbose_name = "Parent Users"
--- Girinie-backend/parent_users/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import LoginView, LogoutView, UserCreateView, UserDeleteView
-urlpatterns = [
-    path("login/", LoginView.as_view(), name='login'),
-    path('logout/', LogoutView.as_view(), name='logout'),
-    path('users/', UserCreateView.as_view(), name='signup'),
-    path('users/me/', UserDeleteView.as_view(), name='delete-account'),
-]
